377f773fe87d7c7264931d24509e5bbb93f86c93
simevskav <simevskav@mk-dir.com>
2026-03-16 09:53:59 +0100

    chore: restructure project and update config

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from './components/LoginForm';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <LoginForm />
+    </div>
+  );
+}
